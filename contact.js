c655dda747f1e14bdc38190d03ad89ef4307377b
tiphaine pinson <tiphaine.pinson@hotmail.fr>
2024-01-16 11:41:55 +0100

    end

--- contact.js
+++ contact.js
@@ -5,6 +5,7 @@ document.getElementById('contactForm').addEventListener('submit', function(event
       .then(function() {
         console.log('SUCCESS!');
         alert('Message envoyé avec succès!');
+        document.getElementById('contactForm').reset();
       }, function(error) {
         console.log('FAILED...', error);
         alert('Erreur lors de l\'envoi du message');
