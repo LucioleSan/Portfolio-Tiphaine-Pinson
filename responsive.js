f2f3e6c3b6fb4ad32a6500c1680202f6b758c28d
tiphaine pinson <tiphaine.pinson@hotmail.fr>
2024-04-09 22:40:33 +0200

    fermeture du menu quand on clique sur un lien

--- responsive.js
+++ responsive.js
@@ -1,4 +1,12 @@
 document.querySelector('.menu-toggle').addEventListener('click', function() {
     this.classList.toggle('active');
-    document.querySelector('nav').style.display = this.classList.contains('active') ? 'flex' : 'none';
-  });
+    document.querySelector('nav').classList.toggle('active');
+});
+
+// Fermer le menu lorsque l'un des liens est cliqué
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', () => {
+        document.querySelector('.menu-toggle').classList.remove('active');
+        document.querySelector('nav').classList.remove('active');
+    });
+});
